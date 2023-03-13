633fd0bd7a1f150369aceb116a457c3db2f8c8cc
Tomasz Piotrowski <tpiotrowski007@gmail.com>
2023-03-13 20:42:09 +0100

    added renderering products

--- src/js/script.js
+++ src/js/script.js
@@ -54,15 +54,41 @@
     ),
   };
   class Product {
-    constructor() {
+    constructor(id, data) {
       const thisProduct = this;
+      
+      thisProduct.id = id;
+      thisProduct.data = data;
+
+      thisProduct.renderInMenu();
       console.log('new product:', thisProduct);
+    }
+    renderInMenu(){
+      const thisProduct = this;
+      /* generate HTML based on template */
+      const generatedHTML = templates.menuProduct(thisProduct.data);
+
+      /* create element using utils.createElementFromHTML */
+      thisProduct.element = utils.createDOMFromHTML(generatedHTML);
+
+      /* find menu container */
+      const menuContainer = document.querySelector(select.containerOf.menu);
+
+      /* add element to menu */
+      menuContainer.appendChild(thisProduct.element);
     }
   }
   const app = {
     initMenu: function () {
-      const testProduct = new Product();
-      console.log('testProduct:', testProduct);
+      const thisApp = this;
+      console.log('thisApp.data:', thisApp.data);
+      for (let productData in thisApp.data.products) {
+        new Product(productData, thisApp.data.products[productData]);
+      }
+    },
+    initData: function () {
+      const thisApp = this;
+      thisApp.data = dataSource;
     },
     init: function () {
       const thisApp = this;
@@ -71,7 +97,7 @@
       console.log('classNames:', classNames);
       console.log('settings:', settings);
       console.log('templates:', templates);
-
+      thisApp.initData();
       thisApp.initMenu();
     },
   };
